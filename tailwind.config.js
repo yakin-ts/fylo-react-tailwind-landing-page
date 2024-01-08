60481c39d119cdf979867dc7c1d7f73acdaedb43
Yakin Teshome <yakin16tm@gmail.com>
2024-01-08 13:28:20 +0300

    [feat]: setup styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,28 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          'intro': 'hsl(217, 28%, 15%)',
+          'main': 'hsl(217, 28%, 15%)',
+          'footer': 'hsl(216, 53%, 9%)', 
+          'testimoial': 'hsl(219, 30%, 18%)', 
+        },
+        accent: {
+          'cyan': 'hsl(176, 68%, 64%)',
+          'blue': 'hsl(198, 60%, 50%)',
+          'light-red': 'hsl(0, 100%, 63%)',
+        },
+        neutral: {
+          'white': 'hsl(0, 0%, 100%)',
+        }
+      },
+      fontFamily: {
+        'raleway': ['Raleway', 'sans-serif'],
+        'open-sans': ['Open Sans', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
